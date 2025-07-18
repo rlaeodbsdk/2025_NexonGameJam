72cc1e2d19c573c87041a4a31ea6f72b51fe446f
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-07-19 02:00:46 +0900

    Feat : 여러가지..

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -8,12 +8,15 @@ public class Node : MonoBehaviour
     private int currentStepIndex = 0;
     private bool isInFirstZone;
     private bool isInSecondZone;
-    private float nodeSpeed = 1;
+    public float nodeSpeed = 1;
     private bool upThrow = false;
+    private bool isReady = false;
+    private NodeManager nodeManager;
     
     private int nodeLine = 0;
 
     private NodeRecipe recipe;
+    private RecipeStep currentStep;
 
     enum NodeState
     {
@@ -24,9 +27,18 @@ public class Node : MonoBehaviour
     private void Start()
     {
         currentState = NodeState.Moving;
+        nodeManager = FindAnyObjectByType<NodeManager>();
+    }
+
+    public void setInstantiateData()
+    {
+        currentStep = recipe.steps[currentStepIndex];
+        GetComponentInChildren<SpriteRenderer>().sprite = recipe.startIngredient;
     }
     private void Update()
     {
+
+
         if (nodeLine == 1) // 왼쪽 노드라인
         {
             transform.position = transform.position + new Vector3(1f * nodeSpeed * Time.fixedDeltaTime, 0, 0); // 노드 생성시 자동움직임 오른쪽으로
@@ -38,32 +50,52 @@ public class Node : MonoBehaviour
         }
 
 
-        RecipeStep currentStep = recipe.steps[currentStepIndex];
 
-  
 
-        if(currentState==NodeState.Flying)
+        if (currentState == NodeState.Flying || isReady ==true)
         {
             return;
         }
         if (isInFirstZone) //첫번째 존
         {
-            if (Input.GetKeyDown(currentStep.key)) //맞는키 누르면 
+            if (Input.GetKeyDown(currentStep.key) && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.W))) //맞는키 누르면 
             {
+                currentStepIndex++;
                 ThrowUpNode();
+                GetComponentInChildren<SpriteRenderer>().sprite = currentStep.sprite;
+                if (currentStepIndex >= recipe.steps.Count) // 더이상 누를게 없다? -> 그러면 완성
+                {
+                    Debug.Log($"[{recipe.dishName}] 완성!");
+                    isReady = true;
+                    return;
+                }
+                currentStep = recipe.steps[currentStepIndex];
+                // 완성이 아니라면
+
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha1)) // 첫번쨰존 넘기기 키 && 현재 Moving  상태
+            else if(Input.GetKeyDown(KeyCode.W))
             {
                 ThrowNode();
             }
+            //잘못된키 누르면 아무 반응도 없다 -> 통과
         }
-        else if(isInSecondZone) //두번쨰 존에선 S를 눌러야 사라짐
+        else if (isInSecondZone) //두번쨰 존에선 S를 눌러야 사라짐
         {
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(currentStep.key) && (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
             {
-                Destroy(this.gameObject);
+                currentStepIndex++;
+                ThrowUpNode();
+                GetComponentInChildren<SpriteRenderer>().sprite = currentStep.sprite;
+                if (currentStepIndex >= recipe.steps.Count) // 더이상 누를게 없다? -> 그러면 완성
+                {
+                    Debug.Log($"[{recipe.dishName}] 완성!");
+                    isReady = true;
+                    return;
+                }
+                currentStep = recipe.steps[currentStepIndex];
+                // 완성이 아니라면
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha3)&&currentState==NodeState.Moving)
+            else if(Input.GetKeyDown(KeyCode.UpArrow))
             {
                 ThrowNode();
             }
@@ -91,8 +123,7 @@ public class Node : MonoBehaviour
 
         if(collision.CompareTag("DestroyZone"))
         {
-            Debug.Log("파괴");
-            Destroy(this.gameObject);
+            StartCoroutine(CompleteGoDestroy());
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -117,6 +148,18 @@ public class Node : MonoBehaviour
     public void GetRecipe(NodeRecipe r)
     {
         recipe = r;
+    }
+
+    IEnumerator CompleteGoDestroy()//완성됐으니 없애는 과정
+    {
+        upThrow = true;
+        currentState = NodeState.Flying;
+        Transform transform = this.transform;
+        transform.position = transform.position + new Vector3(0, 2, 0);
+        GetComponent<ParabolaMover>().StartParabolaMove(transform.position, this.gameObject);
+        yield return new WaitForSecondsRealtime(0.7f);
+        DestroyNode();
+        Destroy(this.gameObject);
     }
     private void ThrowUpNode()
     {
@@ -154,5 +197,10 @@ public class Node : MonoBehaviour
             }
         }
         currentState = NodeState.Moving;
+    }
+
+    private void DestroyNode()
+    {
+        nodeManager.nodeCount--;
     }
 }
