10a2c167ee0b196cd3fbf6f52c521b2d25c09857
seyoo630 <165480675+seyoo630@users.noreply.github.com>
2025-07-18 22:27:28 +0900

    v7_18_3
    상점slotscroll 구현

---
+++ Assets/Scripts/Items/FoodSO.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FoodData", menuName = "Food/Foods")]
+public class FoodSO : ScriptableObject
+{
+    public bool isActive = false;
+    public string foodName;
+    public Sprite foodImage;
+    public int foodPrice;
+    [TextArea]
+    public string foodInfo;
+}
---
+++ Assets/Scripts/Items/FoodSlot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodSlot : MonoBehaviour
+{
+    public Image image;
+    public TextMeshProUGUI foodName;
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI price;
+    public bool isActive;
+
+    private FoodSO foodData;
+
+    public Button slotButton;
+    public void SetData(FoodSO foodData)
+    {
+        this.foodData = foodData;
+        SetValue();
+        slotButton.onClick.RemoveAllListeners();
+        slotButton.onClick.AddListener(() => CustomShopManager.instance.BuyFood(foodData));
+    }
+    public void SetValue()
+    {
+        this.isActive = foodData.isActive;
+        image.sprite = foodData.foodImage;
+        foodName.SetText(foodData.foodName);
+        description.SetText(foodData.foodInfo);
+        price.SetText("price: " + foodData.foodPrice);
+    }
+}
