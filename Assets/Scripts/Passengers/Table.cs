08a814e93a50e9d2ba622e3259a6ec65579504b9
seyoo630 <165480675+seyoo630@users.noreply.github.com>
2025-07-19 14:09:53 +0900

    v_7_19123123123123123

--- Assets/Scripts/Passengers/Table.cs
+++ Assets/Scripts/Passengers/Table.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
-    private NodeManager nodeManager;
+
     private int tableNumber; // 각테이블이 가지는 고유한 ID
     [Header("손님이 앉는 위치")]
     public Transform passengerPoint;
@@ -17,17 +17,10 @@ public class Table : MonoBehaviour
     public void AssignPassenger(Passenger passenger)
     {
         currentPassenger = passenger;
-        if (nodeManager == null)
-            Debug.LogError("nodeManager is NULL!!!");
+       
 
-        if (passenger == null)
-            Debug.LogError("passenger is NULL!!!");
-
-        if (passenger.selectedFood == null)
-            Debug.LogError("passenger.selectedFood is NULL!!!");
-
-        nodeManager = FindFirstObjectByType<NodeManager>();
-        nodeManager.NodeGo(passenger.selectedFood.foodName);
+        var nodeManager = FindFirstObjectByType<NodeManager>();
+        nodeManager.NodeGo(passenger.selectedFood.foodNodeName);
 
 
         passenger.transform.position = passengerPoint.position;
