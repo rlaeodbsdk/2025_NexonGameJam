3d060efcc04d407cd5bd344a8ed5a0ea8607627d
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-07-20 10:54:20 +0900

    1241
    4124

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -33,7 +33,7 @@ public class Bullet : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Table"))
+        if(collision.CompareTag("Table")) // 테이블과 닿았을때
         {
            
             var collidingTable = collision.gameObject.GetComponentInParent<Table>();
@@ -56,13 +56,24 @@ public class Bullet : MonoBehaviour
             Debug.Log(collidingTable.currentPassenger.selectedFood.foodNodeName);
             Debug.Log(recipe);
  
-                if(collidingTable.currentPassenger.selectedFood.foodNodeName == recipe.dishName)
-                {
-                collidingTable.currentPassenger.Exit(true, recipe.currentstepIndex,recipe);
-                collidingTable.ReceivedFood(recipe.steps[recipe.currentstepIndex].sprite, true);
+            if(collidingTable.currentPassenger.selectedFood.foodNodeName == recipe.dishName) // 시킨 음식의 이름과 준비된 레시피의 이름이 같다면
+            {
+                collidingTable.currentPassenger.Exit(true, recipe.currentstepIndex,recipe); // 만족하면서 나가기
+                collidingTable.ReceivedFood(recipe.steps[recipe.currentstepIndex].sprite, true); // 음식받기
                 isShooted = true;   
+             }
+            else // 잘못된 테이블로의 발사?
+            {
+                TableManager tableManager = FindAnyObjectByType<TableManager>();
+                foreach (Table table in tableManager.tables)
+                {
+                    if (recipe.orderTableNumber == table.tableNumber) // 맞는 테이블 찾아서 손님 내쫒기
+                    {
+                        table.currentPassenger.Exit(false, 0, recipe);
+                        table.ResetTable();
+                    }
                 }
-            
+            }
             Debug.Log("테이블과의 접촉");
             
 
